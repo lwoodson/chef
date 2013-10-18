f12744379a1a4a232f0bb853d991736791fe17a3
JJ Asghar <jjasghar@gmail.com>
2013-10-18 11:05:39 -0500

    Update admin.rb
    Comments, alas, if you go down this path you'll need to implement a tad bit more...complex situation.

--- cookbooks/core/recipes/admin.rb
+++ cookbooks/core/recipes/admin.rb
@@ -3,6 +3,9 @@ package 'libcurl4-openssl-dev'
 
 gem_package "ruby-shadow"
 
+# Note: you might want to seriosuly consider http://community.opscode.com/cookbooks/sudo. 
+# But with that you'll want to look into berkshelf. http://berkshelf.com/
+# Just a thought :)
 template "/etc/sudoers" do
   source "sudoers.erb"
   mode 0440
